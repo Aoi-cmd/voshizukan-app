89f832c10f32410abc8e309605b38d49465666e2
Aoi-cmd <promise.restriction@gmail.com>
2025-05-06 17:17:14 +0900

    初回コミット：V推し図鑑アプリ

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'bcrypt' # パスワードのハッシュ化に使用
+gem 'rackup'
---
+++ app.rb
@@ -0,0 +1,54 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'bcrypt'
+
+enable :sessions
+
+# 仮のユーザーデータ（本番ではDB推奨）
+USERS = {
+  "testuser" => BCrypt::Password.create("password123")
+}
+
+# トップページ（テスト用）
+get '/' do
+  "Sinatraが動作しています!この世界 in my world..."
+end
+
+# ログインフォーム表示
+get '/login' do
+  erb :login
+end
+
+# ログイン処理
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  if USERS[username] && BCrypt::Password.new(USERS[username]) == password
+    session[:user] = username
+    redirect '/index'
+  else
+    @error = "ユーザー名またはパスワードが間違っています"
+    erb :login
+  end
+end
+
+# ログイン後のホーム画面
+get '/home' do
+  redirect '/login' unless session[:user]
+  erb :home
+end
+
+# ログアウト処理
+get '/logout' do
+  session.clear
+  redirect '/login'
+end
+
+
+set :public_folder, File.dirname(__FILE__)
+
+get '/index' do
+  redirect '/login' unless session[:user]
+  send_file File.join(settings.public_folder, 'index.html')
+end
